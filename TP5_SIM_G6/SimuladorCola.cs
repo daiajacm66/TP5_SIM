34368b79704b39db335aef89cc4ed82007c86a84
Candela Vega 87345 <102198046+vegacande@users.noreply.github.com>
2023-10-24 00:43:23 -0300

    version 1.1 - Personalización, clases

--- TP5_SIM_G6/SimuladorCola.cs
+++ TP5_SIM_G6/SimuladorCola.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace TP5_SIM_G6
 {
-    public partial class Form1 : Form
+    public partial class SimuladorCola : Form
     {
-        public Form1()
+        public SimuladorCola()
         {
             InitializeComponent();
         }
